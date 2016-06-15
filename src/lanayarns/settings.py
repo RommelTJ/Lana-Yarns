d9bf7a8984a8de7a031189c7067d0c39a03f021b
Rommel Rico <rommeltj@gmail.com>
2016-06-14 21:06:29 -0700

    Fixing issues with template and email registration

--- src/lanayarns/settings.py
+++ src/lanayarns/settings.py
@@ -133,26 +133,27 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "static_root")
-
+#Team manages
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static_in_pro", "our_static"),
-    #os.path.join(BASE_DIR, "static_in_env"),
-    #'/var/www/static/',
+    #Dirs
+    os.path.join(BASE_DIR, "static"),
 )
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "media_root")
+#Team doesn't manage
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "staticfiles") #CDN
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media") #CDN
+PROTECTED_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "protected") #CDN
 
-
-
-#Crispy FORM TAGs SETTINGS
+#Crispy Form Tags Settings
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
-
-#DJANGO REGISTRATION REDUX SETTINGS
+#Django Registration Redux Settings
 ACCOUNT_ACTIVATION_DAYS = 7
+
 REGISTRATION_AUTO_LOGIN = True
+
 SITE_ID = 1
+
 LOGIN_REDIRECT_URL = '/'
