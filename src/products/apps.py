c23eea464b221086f57a7a8ce679695c2e319f22
Rommel Rico <rommeltj@gmail.com>
2016-06-18 07:42:28 -0700

    Adding post_save signal for variations

--- src/products/apps.py
+++ src/products/apps.py
@@ -2,6 +2,8 @@ from __future__ import unicode_literals
 
 from django.apps import AppConfig
 
-
 class ProductsConfig(AppConfig):
     name = 'products'
+
+    def ready(self):
+        from . import signals
