45e7a7801fa5a8aa4850cbe6e59a9f28d77bb1eb
Rommel Rico <rommeltj@gmail.com>
2016-06-18 18:17:26 -0700

    Adding basic formset for inventory

--- src/products/urls.py
+++ src/products/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import ProductDetailView, ProductListView
+from .views import ProductDetailView, ProductListView, VariationListView
 
 urlpatterns = [
     url(r'^$', ProductListView.as_view(), name='product'),
     url(r'^(?P<pk>\d+)/$', ProductDetailView.as_view(), name='product_detail'),
+    url(r'^(?P<pk>\d+)/inventory/$', VariationListView.as_view(), name='product_inventory'),
 ]
