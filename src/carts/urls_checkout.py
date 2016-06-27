120af07aee0bf42447d25e4b552c72a3fac9c2b0
Rommel Rico <rommeltj@gmail.com>
2016-06-27 08:01:17 -0700

    Add useraddress model & address select view

--- src/carts/urls_checkout.py
+++ src/carts/urls_checkout.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
+from orders.views import AddressSelectFormView
 from .views import CheckoutView
 
 urlpatterns = [
     url(r'^$', CheckoutView.as_view(), name='checkout'),
+    url(r'^address/$', AddressSelectFormView.as_view(), name='order_address')
 ]
--- src/orders/admin.py
+++ src/orders/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import UserCheckout
+from .models import UserCheckout, UserAddress
 
 # Register your models here.
 
 admin.site.register(UserCheckout)
+admin.site.register(UserAddress)
