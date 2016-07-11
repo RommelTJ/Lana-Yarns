c8737d9bd4d3178306508b95efcaafa3e45e438d
Rommel Rico <rommeltj@gmail.com>
2016-07-10 18:31:54 -0700

    Add script to test api auth method

---
+++ src/lanayarns/api_test.py
@@ -0,0 +1,30 @@
+import requests
+import json
+
+base_url = 'http://localhost:8000/api/'
+
+login_url = base_url + 'auth/token/'
+products_url = base_url + 'products/'
+
+# requests.post(login_url, data=None, headers=None, params=None)
+
+# Auth test
+data = {
+    'username': 'testuser',
+    'password': 'test1234',
+}
+
+login_r = requests.post(login_url, data=data)
+
+json_data = login_r.json() #login_r.text
+print(json.dumps(json_data, indent=2))
+
+token = json_data['token']
+
+# Retrieve products test
+headers = {
+    'Authorization': 'JWT %s' % (token)
+}
+p_r = requests.get(products_url, headers=headers)
+prod_json_data = p_r.json()
+print (json.dumps(prod_json_data, indent=2))
